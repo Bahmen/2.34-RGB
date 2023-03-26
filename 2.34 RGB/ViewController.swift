249781712b187558388efa0576d995d2a1a76853
Bahmen <4181035@gmail.com>
2023-03-26 18:01:43 +0600

    Initial Commit

---
+++ 2.34 RGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.34 RGB
+//
+//  Created by Bax Bax on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
