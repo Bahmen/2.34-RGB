867758003b3bbcc21d1d97ecbcd4d1a830004488
Bahmen <4181035@gmail.com>
2023-03-26 20:41:37 +0600

    final but not fix bzzzz

--- 2.34 RGB/ViewController.swift
+++ 2.34 RGB/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewRGB: UIView!
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViewRGB()
+
+        sliderActionRed()
+        sliderActionGreen()
+        sliderActionBlue()
     }
 
+    @IBAction func sliderActionRed() {
+        labelRed.text = (round(sliderRed.value * 100) / 100).formatted()
+        viewRGB.backgroundColor = (UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1))
+    }
+    
+    @IBAction func sliderActionBlue() {
+        labelBlue.text = (round(sliderBlue.value * 100) / 100).formatted()
+        viewRGB.backgroundColor = (UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1))
+    }
 
+    @IBAction func sliderActionGreen() {
+        labelGreen.text = (round(sliderGreen.value * 100) / 100).formatted()
+        viewRGB.backgroundColor = (UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1))
+    }
+
+    
+    private func setupViewRGB() {
+        viewRGB.layer.cornerRadius = 10
+    }
+
+  
 }
 
